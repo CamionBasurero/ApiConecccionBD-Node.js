e5e27fa0f6ef2b0527670aa8d7bca6f50f4be16c
juan pablo <92799801+CamionBasurero@users.noreply.github.com>
2024-09-12 11:18:39 -0300

    Add files via upload

--- Index.js
+++ Index.js
@@ -71,10 +71,10 @@ app.get('/RecetaSeleccionada', async (req, res) => {
                 const TemperaturaMacerado = receta.Temperatura_Macerado;
                 const TemperaturaCoccion = receta.Temperatura_De_Coccion;
 
-                const TiempoMacerado = parseInt(receta.Tiempo_de_macerado);  // Convertir a int
+                const TiempoMacerado = parseFloat(receta.Tiempo_de_macerado);  // Convertir a int
                 const TiempoCoccion = parseInt(receta.Tiempo_de_Coccion);    // Convertir a int
                 const TiempoClarificado = parseInt(receta.Tiempo_de_clarificado);  // Convertir a int
-                const TiempoWhirlpool = parseInt(receta.TiempoWhirlpool);  // Convertir a int
+                const TiempoWhirlpool = parseInt(receta.Whirlpool);  // Convertir a int
 
                 //Otras variables
                 const CantLitros = receta.Litros;
