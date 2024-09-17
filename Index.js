cb66fcc49a30a5bcdda58004d1993bc8e862609d
juan pablo <92799801+CamionBasurero@users.noreply.github.com>
2024-09-17 13:11:58 -0300

    Add files via upload

--- Index.js
+++ Index.js
@@ -142,17 +142,7 @@ app.post('/ActualizarReceta', async (req, res) => {
                 params.push({ name: 'TiempoClarificado', value: tiempoClarificadoTranscurrido, type: sql.VarChar });
             }
 
-            if (E_Hervido !== undefined && E_Hervido !== null) {
-                console.log(`Estado hervido: ${E_Hervido}`);
-                query += "Estado_Hervido = @E_Hervido, ";
-                params.push({ name: 'E_Hervido', value: E_Hervido, type: sql.Bit });
-            }
-
-            if (E_Macerado !== undefined && E_Macerado !== null) {
-                console.log(`Recibido E_Macerado: ${E_Macerado}`);
-                query += "Estado_Macerado = @E_Macerado, ";
-                params.push({ name: 'E_Macerado', value: E_Macerado, type: sql.Bit });
-            }
+            
             // Eliminar la última coma y agregar la condición WHERE
             query = query.slice(0, -2) + " WHERE Nombre_De_Receta = @NombreReceta";
             params.push({ name: 'NombreReceta', value: NombreReceta, type: sql.VarChar });
