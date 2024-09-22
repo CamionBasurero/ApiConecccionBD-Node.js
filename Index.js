1a04706c1b0d2a83d766c1bee06dcc8a6f3abd90
juan pablo <92799801+JuanPabloGay@users.noreply.github.com>
2024-09-22 17:04:05 -0300

    Add files via upload

--- Index.js
+++ Index.js
@@ -1,20 +1,20 @@
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const sql = require('mssql'); // Asegúrate de que estás usando 'mssql'
-const moment = require('moment');
+import express from 'express';
+import { json } from 'body-parser';
+import cors from 'cors';
+import { connect, Float, VarChar, Bit } from 'mssql';
+import moment from 'moment';
 
 const app = express();
-app.use(bodyParser.json());
+app.use(json());
 app.use(cors());
 
 // Configuración de la base de datos
 const DBConfig = {
-    user: process.env.DB_USER || 'juanpi123_SQLLogin_1',
-    password: process.env.DB_PASSWORD || 'b9x48xw44a',
-    server: process.env.DB_SERVER || 'DB_PlantaDC.mssql.somee.com', 
-    database: process.env.DB_DATABASE || 'DB_PlantaDC',
+    user:'juanpi123_SQLLogin_1',
+    password:'b9x48xw44a',
+    server:'DB_PlantaDC.mssql.somee.com', 
+    database:'DB_PlantaDC',
     options: {
         encrypt: true,
         enableArithAbort: true,
@@ -26,7 +26,7 @@ const DBConfig = {
 // Función para conectar a la base de datos
 async function conectarDB() {
     try {
-        const pool = await sql.connect(DBConfig);
+        const pool = await connect(DBConfig);
         console.log('Conectado a la base de datos');
         return pool;
     } catch (error) {
@@ -108,58 +108,58 @@ app.post('/ActualizarReceta', async (req, res) => {
             // Agregar parámetros dinámicamente si están presentes
             if (volumen !== undefined && volumen !== null) { 
                 query += "Litros_Llenado = @Volumen, ";
-                params.push({ name: 'Volumen', value: volumen, type: sql.Float });
+                params.push({ name: 'Volumen', value: volumen, type: Float });
             }
 
             if (temperaturaHervidoReal !== undefined && temperaturaHervidoReal !== null) {
                 query += "Temp_Hervido_Real = @TemperaturaHervido, ";
-                params.push({ name: 'TemperaturaHervido', value: temperaturaHervidoReal, type: sql.Float });
+                params.push({ name: 'TemperaturaHervido', value: temperaturaHervidoReal, type: Float });
             }
 
             if (temperaturaMaceradoReal !== undefined && temperaturaMaceradoReal !== null) {
                 query += "Temp_Macerado_Real = @TemperaturaMacerado, ";
-                params.push({ name: 'TemperaturaMacerado', value: temperaturaMaceradoReal, type: sql.Float });
+                params.push({ name: 'TemperaturaMacerado', value: temperaturaMaceradoReal, type: Float });
             }
 
             if ( tiempoMaceradoTranscurrido !== null) {
                 query += "Tiempo_Macerado_Transcurrido = @TiempoMacerado, ";
-                params.push({ name: 'TiempoMacerado', value: tiempoMaceradoTranscurrido, type: sql.VarChar });
+                params.push({ name: 'TiempoMacerado', value: tiempoMaceradoTranscurrido, type: VarChar });
             }
            
             if ( tiempoClarificadoTranscurrido !== null) {
                 query += "Tiempo_Recirculado_Transcurrido = @TiempoClarificado, ";
-                params.push({ name: 'TiempoClarificado', value: tiempoClarificadoTranscurrido, type: sql.VarChar });
+                params.push({ name: 'TiempoClarificado', value: tiempoClarificadoTranscurrido, type: VarChar });
             }
             
             if (E_Hornalla !== undefined && E_Hornalla !== null) {
                 query += "Estado_Calefaccion_Hervido = @E_Hornalla, ";
-                params.push({ name: 'E_Hornalla', value: E_Hornalla ? 1 : 0, type: sql.Bit });  // Convertir bool a bit
+                params.push({ name: 'E_Hornalla', value: E_Hornalla ? 1 : 0, type: Bit });  // Convertir bool a bit
             }
 
             if (E_R_Calefaccion !== undefined && E_R_Calefaccion !==null ) {
                 query += "Estado_Calefaccion_Macerado = @E_R_Calefaccion, ";
-                params.push({ name: 'E_R_Calefaccion', value: E_R_Calefaccion ? 1 : 0, type: sql.Bit });
+                params.push({ name: 'E_R_Calefaccion', value: E_R_Calefaccion ? 1 : 0, type: Bit });
             }
 
             if (E_Hervido !== undefined && E_Hervido !== null) {
                 query += "Estado_Hervido = @E_Hervido, ";
-                params.push({ name: 'E_Hervido', value: E_Hervido ? 1 : 0, type: sql.Bit });  // Convertir bool a bit
+                params.push({ name: 'E_Hervido', value: E_Hervido ? 1 : 0, type: Bit });  // Convertir bool a bit
             }
 
             if (E_Macerado !== undefined && E_Macerado!==null ) {
                 query += "Estado_Macerado = @E_Macerado, ";
-                params.push({ name: 'E_Macerado', value: E_Macerado ? 1 : 0, type: sql.Bit });
+                params.push({ name: 'E_Macerado', value: E_Macerado ? 1 : 0, type: Bit });
             }
             
             if (Receta_Seleccionada !== undefined && Receta_Seleccionada!==null ) {
                 
                 query += "Receta_Seleccionada = @Receta_Seleccionada, ";
-                params.push({ name: 'Receta_Seleccionada', value: Receta_Seleccionada ? 1 : 0, type: sql.Bit });
+                params.push({ name: 'Receta_Seleccionada', value: Receta_Seleccionada ? 1 : 0, type: Bit });
             }
             
             // Eliminar la última coma y agregar la condición WHERE
             query = query.slice(0, -2) + " WHERE Nombre_De_Receta = @NombreReceta";
-            params.push({ name: 'NombreReceta', value: NombreReceta, type: sql.VarChar });
+            params.push({ name: 'NombreReceta', value: NombreReceta, type: VarChar });
 
             // Preparar la consulta
             const request = pool.request();
